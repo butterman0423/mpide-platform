d27d5902745027ba25037ffd04a89f4b223749f1
Nathaniel Escaro <butterman423@gmail.com>
2025-12-27 17:17:14 -0500

    Initial commit

---
+++ src/main/java/com/mpide/mpide_platform/MpidePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.mpide.mpide_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpidePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpidePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpide/mpide_platform/MpidePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpide.mpide_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpidePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
