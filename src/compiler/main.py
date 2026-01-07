7f7a440aad6eadedf25a4ba19041c32410654c0e
EsatAdiloglu <esatadiloglu16@gmail.com>
2026-01-06 23:14:38 -0500

    feat: added functions

--- src/compiler/main.py
+++ src/compiler/main.py
@@ -1,4 +1,33 @@
 import socket
+import tempfile
+import json
+import os
+
+
+
+
+def load_config(config_path='config.json'):
+    with open(config_path,'r') as f:
+        return json(f)
+
+def read_files(conn):
+    #TODO: Read the files from the client socket
+    pass
+
+def make_object_files(file = None):
+    #TODO: run avr-gcc on all of the .c files, and save them to the tmp directory
+    if not file:
+        return
+    pass
+
+def link_files():
+    #TODO: run avr-gcc -mmcu {chip archieture} {all of the .o files} {.elf file}
+    pass
+
+
+def compile_code():
+    pass
+
 def main():
     #Assuming we're using IPv4
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,7 +41,9 @@ def main():
         
         while True:
             client_socket, addr = server_socket.accept()
-            print(f"Connected to {client_socket.getpeername()} from {addr}")
+            print(f"Connected to {addr}")
+            
+            
     
     
     
