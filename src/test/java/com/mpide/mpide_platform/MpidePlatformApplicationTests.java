c6bc97d924c6de22063c6764d7ec5043bffee3e0
Nathaniel Escaro <butterman423@gmail.com>
2026-01-06 23:11:56 -0500

    add linter and formatter

--- src/test/java/com/mpide/mpide_platform/MpidePlatformApplicationTests.java
+++ src/test/java/com/mpide/mpide_platform/MpidePlatformApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MpidePlatformApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
