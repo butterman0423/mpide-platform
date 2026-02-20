638c5cb04a9c0a99c9645a0fe2b51d7b5214f09a
EsatAdiloglu <esatadiloglu16@gmail.com>
2026-02-20 02:39:12 -0500

    feat: started 4.1; hate css

--- src/mpide-frontend/src/app/app.component.ts
+++ src/mpide-frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FileManagementComponent } from "./components/file-management/file-management.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FileManagementComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/mpide-frontend/src/app/components/file-management/file-management.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+
+@Component({
+  selector: 'app-file-management',
+  imports: [NzDividerModule],
+  templateUrl: './file-management.component.html',
+  styleUrls: ['./file-management.component.css']
+})
+export class FileManagementComponent{
+}
