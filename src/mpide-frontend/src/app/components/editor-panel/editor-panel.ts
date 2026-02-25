41e1907888ea70a8314a57500e98e42898b5212e
Saikarthik M <ganoworc345@gmail.com>
2026-02-24 20:16:22 -0500

    Start editor panel component

---
+++ src/mpide-frontend/src/app/components/editor-panel/editor-panel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editor-panel',
+  imports: [],
+  templateUrl: './editor-panel.html',
+  styleUrl: './editor-panel.css',
+})
+export class EditorPanel {
+
+}
