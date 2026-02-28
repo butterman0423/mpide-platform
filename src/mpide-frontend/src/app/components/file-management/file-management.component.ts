3c5de5ea843da4f6d8fd3bfbb4c70d6487f551d7
Saikarthik M <ganoworc345@gmail.com>
2026-02-28 15:48:08 -0500

    Pushing change

--- src/mpide-frontend/src/app/components/file-management/file-management.component.ts
+++ src/mpide-frontend/src/app/components/file-management/file-management.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, inject, signal, ViewChild } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { FileCardComponent } from '../file-card/file-card.component';
