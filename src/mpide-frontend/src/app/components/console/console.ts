d1d34aa1613fda41360c2c180c6cfa516f64c01a
Butter - ThinkWin <butterman423@gmail.com>
2026-02-28 19:24:49 -0500

    impl console

--- src/mpide-frontend/src/app/components/console/console.ts
+++ src/mpide-frontend/src/app/components/console/console.ts
@@ -1,11 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef, input } from '@angular/core';
+import { NzIconDirective } from "ng-zorro-antd/icon";
+
+export type MessageData = {
+  ty: "LOG"
+  dat: GeneralMessageData
+} | {
+  ty: "ERROR"
+  dat: ErrorMessageData
+}
+
+export interface GeneralMessageData {
+  ty: "SYSTEM" | "SERVER"
+  msg: string
+}
+export interface ErrorMessageData {
+  header: string
+  msgs: string[]
+}
 
 @Component({
   selector: 'app-console',
-  imports: [],
+  imports: [
+    NzIconDirective, 
+    forwardRef(() => ServerMessage),
+    forwardRef(() => SystemMessage),
+    forwardRef(() => ErrorMessage)
+  ],
   templateUrl: './console.html',
   styleUrl: './console.css',
 })
 export class Console {
-  
+  msgs = input.required<MessageData[]>()
+}
+
+@Component({
+  selector: "msg-server",
+  imports: [],
+  templateUrl: "./messages/msg-server.html",
+  styleUrl: "./console.css"
+})
+export class ServerMessage {
+  msg = input.required<string>();
+
+}
+
+@Component({
+  selector: "msg-system",
+  imports: [],
+  templateUrl: "./messages/msg-sys.html",
+  styleUrl: "./console.css"
+})
+export class SystemMessage {
+  msg = input.required<string>();
+}
+
+@Component({
+  selector: "msg-error",
+  imports: [forwardRef(() => DollarLine)],
+  templateUrl: "./messages/msg-error.html",
+  styleUrl: "./console.css"
+})
+export class ErrorMessage {
+  header = input.required<string>();
+  msgs = input.required<string[]>();
+}
+
+@Component({
+  selector: "dollar-line",
+  imports: [],
+  templateUrl: "./dollar-line.html",
+  styleUrl: "./console.css"
+})
+export class DollarLine {
+  msg = input.required<string>();
 }
