6701bd38bdf25a8fdd00ea403f0e2fec1c6a9769
EsatAdiloglu <esatadiloglu16@gmail.com>
2026-04-07 00:18:50 -0400

    fixed linting issues

--- src/frontend/src/app/services/file-compiler.ts
+++ src/frontend/src/app/services/file-compiler.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable} from '@angular/core';
 import { IdeFile } from '../models/file.model';
-import { HttpClient, HttpHeaders, HttpParams, HttpStatusCode } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpStatusCode } from '@angular/common/http';
 import { environment } from '../../environments/environment.development';
 import { FileStoreService } from './file-store';
 
