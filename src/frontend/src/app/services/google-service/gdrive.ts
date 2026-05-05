c80093c5d25cf38ffd3477046f12db7e827646a2
Ryan Eshan <ryaneshan04@gmail.com>
2026-05-05 13:46:09 -0400

    google import added

--- src/frontend/src/app/services/google-service/gdrive.ts
+++ src/frontend/src/app/services/google-service/gdrive.ts
@@ -7,6 +7,7 @@ type TokenResponse = google.accounts.oauth2.TokenResponse
 
 interface GApiWindow extends Window {
     google: typeof google
+    gapi: typeof gapi
 }
 
 @Injectable({
@@ -17,19 +18,32 @@ export class GDriveService {
     private accessToken: string | undefined
     private serviceLoaded = false
     private document: Document = inject(DOCUMENT)
+    private appId = environment.googleApiClientId.split('-')[0];
+
+    private pendingAuthForCallback?: () => void;
 
     constructor() {
         fromEvent(this.document.defaultView as Window, 'load').subscribe((e) => {
-            const { google } = e.currentTarget as GApiWindow
+            const { google, gapi } = e.currentTarget as unknown as GApiWindow;
             this.tokenClient = google.accounts.oauth2.initTokenClient({
                 client_id: environment.googleApiClientId,
-                scope: [
-                    "https://www.googleapis.com/auth/drive"
-                ].join(" "),
-                callback: this._handleTokenResponse
-            })
+                scope: ["https://www.googleapis.com/auth/drive.readonly"].join(" "),
+                callback: (resp: TokenResponse) => {
+                    this._handleTokenResponse(resp);
 
-            this.serviceLoaded = true
+                    if (this.accessToken && this.pendingAuthForCallback){
+                        this.pendingAuthForCallback();
+                        this.pendingAuthForCallback = undefined;
+                    }
+                }
+            });
+
+            gapi.load('client:picker', async() => {
+                await gapi.client.init({
+                    discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
+                });
+                this.serviceLoaded = true
+            })
         })
     }
 
@@ -39,14 +53,84 @@ export class GDriveService {
             return
         }
         this.accessToken = tokRes.access_token
+        console.log("auth worked")
+    }
+
+    public isAuthenticated(): boolean {
+        return !!this.accessToken;
     }
 
     public isServiceLoaded(): boolean {
         return this.serviceLoaded
     }
+    
+    public requestAuth(callback?: () => void): void {
+        if (!this.tokenClient) return;
 
-    public requestAuth(): void {
-        if (!this.tokenClient) return
-        this.tokenClient.requestAccessToken()
+        if (callback) {
+            this.pendingAuthForCallback = callback;
+        }
+        this.tokenClient.requestAccessToken();
+    }
+
+    public openFolderPicker(onFolderSelected: (folderId: string, folderName: string) => void): void {
+        if (!this.accessToken) {
+            this.requestAuth(() => this.openFolderPicker(onFolderSelected));
+            return;
+        }
+
+        const {google} = this.document.defaultView as unknown as GApiWindow;
+        
+        const view = new google.picker.DocsView(google.picker.ViewId.FOLDERS)
+            .setIncludeFolders(true)
+            .setSelectFolderEnabled(true)
+            .setMimeTypes('application/vnd.google-apps.folder');
+
+        const picker = new google.picker.PickerBuilder()
+            .addView(view)
+            .setOAuthToken(this.accessToken)
+            .setAppId(this.appId)
+            .setCallback((data: google.picker.ResponseObject) => {
+                if (data.action === google.picker.Action.PICKED && data.docs && data.docs.length > 0) {
+                    const document = data.docs[0];
+                    onFolderSelected(document.id || '', document.name || 'Unknown Folder');
+                }
+            })
+            .build();
+
+        picker.setVisible(true);
+    }
+
+    public async fetchProjectFiles(folderId: string): Promise<{name: string, content: string}[]> {
+        if (!this.accessToken) throw new Error("Not authenticated");
+        const { gapi } = this.document.defaultView as unknown as GApiWindow;
+
+        const query = `'${folderId}' in parents and (name contains '.c' or name contains '.cpp' or name contains '.h') and trashed = false`;
+        
+        const response = await gapi.client.drive.files.list({
+            q: query,
+            fields: 'files(id, name)',
+            spaces: 'drive'
+        });
+
+        const rawFiles = response.result.files || [];
+        const validExtensions = ['.c', '.cpp', '.h'];
+        const files = rawFiles.filter(file => 
+            file.name && validExtensions.some(ext => file.name!.toLowerCase().endsWith(ext))
+        );
+        const loadedFiles: {name: string, content: string}[] = [];
+
+        for (const file of files) {
+            const fileData = await gapi.client.drive.files.get({
+                fileId: file.id || '',
+                alt: 'media'
+            });
+            loadedFiles.push({
+                name: file.name || 'Unknown_File',
+                content: fileData.body
+            });
+        }
+
+        return loadedFiles;
     }
 }
